956f75cdfd35c3f2fe759adb1220161f9ed4552e
knox <111348305+encryptedknoxx@users.noreply.github.com>
2022-12-24 15:42:49 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -39,8 +39,8 @@ You can use /rename command after receiving file to rename it with custom thumbn
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
 
     ERROR_YTDLP = "please report this issue on group ."
-ABOUT_USER = Name : Url uploader Bot
-             Channel: @knoxbots
-              Server: vps
-              Launguage: python3
-              Developer : @ace_knox
+ABOUT_USER ="""🥏Name : Url uploader Bot
+               🥏Channel: @knoxbots
+               🥏Server: vps
+               🥏Launguage: python3
+               🔮Developer : @ace_knox """
